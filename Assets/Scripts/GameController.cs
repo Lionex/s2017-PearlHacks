8522def392f834ecc48bbc11e866b3155d4c6284
Gwen Lofman <Gwen@Lofman.co>
2017-02-12 00:32:38 -0500

    Create GameController to spawn bubbles
    Used to continuously spawn bubbles that will fall into the play
    area.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject bubble;	
+	public Vector3 spawnValues;
+	public float spawnJitter;
+	public float spawnWait;
+	public float startWait;
+	public float timeVariance;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update () {
+		if (false) { // HACK: need the couroutine to end only under a condition to be determined.
+			StopCoroutine (SpawnWaves ());
+		}
+	}
+
+	// Update is called once per frame
+	IEnumerator SpawnWaves () {
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			Vector3 spawnPosition = new Vector3 (spawnValues.x + Random.Range(-spawnJitter,spawnJitter),spawnValues.y,spawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			Debug.Log("Create bubble.");
+			Instantiate(bubble, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds (spawnWait + Random.Range(-spawnJitter, spawnJitter));
+		}
+	}
+}
